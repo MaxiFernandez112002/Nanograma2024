1b63bec7296b6f4c5ff9f12c2033410c25088cb3
MartinAgnello <martinagnello56@gmail.com>
2024-04-21 17:31:28 -0300

    agregados comentarios para entender mejor el codigo

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -1,7 +1,4 @@
-:- module(proylcc,
-	[  
-		put/8
-	]).
+:- module(proylcc,[ put/8 ]).
 
 :-use_module(library(lists)).
 
@@ -10,6 +7,11 @@
 % replace(?X, +XIndex, +Y, +Xs, -XsY)
 %
 % XsY is the result of replacing the occurrence of X in position XIndex of Xs by Y.
+/*
+CB: cuando el indice es 0 entonces reemplaza el primer elemento de la lista [X|Xs] por la Y, quedando como dato de salida [Y|Ys]
+CR: Llama recursivamente disminuyendo el indice hasta que el indice sea 0 y el 1er elem sea X 
+Pregunta: Porque siempre reemplaza al 1er elem de la lista?
+*/
 
 replace(X, 0, Y, [X|Xs], [Y|Xs]).
 
@@ -21,7 +23,15 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %
 % put(+Content, +Pos, +RowsClues, +ColsClues, +Grid, -NewGrid, -RowSat, -ColSat).
-%
+/*
+row = fila
+put/8 actualiza una matriz (Grid) en una posición específica con un nuevo valor (Content), la matriz de salida sera NewGrid
+content contenido que es # o X
+Pos es una lista [Fila, Columna], indicando la posición donde se desea colocar Contenido
+row clues y col clues son las pistas de las filas y col respectivamente
+FilaSat es 1 si las fila de Pos satisface las pistas asociadas, y 0 en caso contrario, ColSat es analogo
+
+*/
 
 put(Content, [RowN, ColN], _RowsClues, _ColsClues, Grid, NewGrid, 0, 0):-
 	% NewGrid is the result of replacing the row Row in position RowN of Grid by a new row NewRow (not yet instantiated).
