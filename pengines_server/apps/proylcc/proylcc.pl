dd58d1c8f505bc11a35cdace84e2a7ac32e84c74
MartinAgnello <martinagnello56@gmail.com>
2024-04-28 19:49:31 -0300

    añadidos los verificar fila y columna

--- pengines_server/apps/proylcc/proylcc.pl
+++ pengines_server/apps/proylcc/proylcc.pl
@@ -2,8 +2,7 @@
 
 :-use_module(library(lists)).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+
 % replace(?X, +XIndex, +Y, +Xs, -XsY)
 %
 % XsY is the result of replacing the occurrence of X in position XIndex of Xs by Y.
@@ -21,8 +20,11 @@ replace(X, XIndex, Y, [Xi|Xs], [Xi|XsY]):-
     XIndexS is XIndex - 1,
     replace(X, XIndexS, Y, Xs, XsY).
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%
+
+
+
+
+
 % put(+Contenido, +Pos, +FilasClues, +ColsClues, +Grilla, -NuevaGrilla, -FilaSat, -ColSat).
 /*
 Fila = fila
@@ -34,18 +36,116 @@ FilaSat es 1 si las fila de Pos satisface las pistas asociadas, y 0 en caso cont
 cambiar los 0 por 2 var 
 */
 
-put(Contenido, [FilaNumero, ColNumero], _PistasFilas, _PistasColumnas, Grilla, NuevaGrilla, FilaSat, ColSat):-
+put(Contenido, [FilaNumero, ColNumero], PistasFilas, PistasColumnas, Grilla, NuevaGrilla, FilaSat, ColSat):-
+
 	% NuevaGrilla es el resultado de reemplazar la fila vieja por la nueva modificada
 	replace(Fila, FilaNumero, NewFila, Grilla, NuevaGrilla),	%lo q hace este 1er replace es conseguir la fila q se busca y la misma pero modificada
 
-	% NewFila is the result of replacing the Celda Celda in position ColNumero of Fila by _,
-	% if Celda matches Contenido (Celda is instantiated in the call to replace/5).	
-	% Otherwise (;)
-	% NewFila is the result of replacing the Celda in position ColNumero of Fila by Contenido (no matter its Contenido: _Celda).			
 	(replace(Celda, ColNumero, _, Fila, NewFila),
 	Celda == Contenido		/*Si estas pintando y lo presionas de nuevo lo despinta*/
 		;
-	replace(_Celda, ColNumero, Contenido, Fila, NewFila)).
+	replace(_Celda, ColNumero, Contenido, Fila, NewFila)),
+
+	verificar_fila(FilaNumero,PistasFilas,NuevaGrilla,FilaSat),		% Verifica si para la fila se cumple lo indicado en su respectiva lista fila de pistas, 
+																	% en caso de cumplirse FilaSat es 1, caso contrario 0.
+	verificar_columna(ColNumero,PistasColumnas,NuevaGrilla, ColSat).% Verifica si para la columna se cumple lo indicado en su respectiva lista columna de pistas, 
+																	% en caso de cumplirse ColSat es 1, caso contrario 0.
+
+
+
+
+
+
+/*
+verificar_fila(+Posicion,+ListaPistas,+GrillaRes,-N)
+verifica que la fila/columna tenga sus pistas satisfechas.
+1==TRUE, 0==FALSE
+Ejemplo: verificar_fila(2, 4, NuevaGrilla, 0/1)
+	
+	nth0(+Indice, +Lista, -ElemEnPosIndex)
+
+Primero se entra a verificar_fila(IndiceFila, PistasFilas, GrillaRes, 1)
+y en caso de que no se verifique el predicado se va a verificar_fila(_,_,_,0).
+*/
+
+verificar_fila(IndiceFila, PistasFilas, GrillaRes, 1):-
+	nth0(IndiceFila, PistasFilas, PistaDeFila),				% Obtiene las pistas (o la pista) de la fila 
+	nth0(IndiceFila, GrillaRes, Filadegrilla),				% Obtiene la fila correspondiente a la posicion fila, de la grilla
+    verificar_pistas_en_lista(PistaDeFila, Filadegrilla).	% Verifica que la fila de la grilla cumpla con las pistas de la misma
+
+verificar_fila(_,_,_,0).									% Si termino de recorrer ambas listas y no se verifica las pistas en lista, retorna 0.
+
+
+
+
+
+/*
+obtener_columna_acum(+Grilla, +NumCol, -ColumnaResultante)
+CB: cuando la lista de filas está vacía.
+CR: Si la grilla no esta vacia, entonces en la 1er lista se busca el elem que se encuentre en la columna deseada y se lo agrega al comienzo de la columna de salida,
+luego se llama recursivamente con la cola de la grilla
+*/
+obtener_columna(Grilla, Col, Columna) :-
+	obtener_columna_acum(Grilla, Col, [], Columna).
+
+obtener_columna_acum([], _, ColumnaAcum, ColumnaAcum).  % 
+
+obtener_columna_acum([Fila|Grilla], Col, ColumnaAcum, Columna) :-
+	nth0(Col, Fila, Elem),  % Obtenemos el elemento en la posición Col de la fila actual.
+	obtener_columna_acum(Grilla, Col, [Elem|ColumnaAcum], Columna).  % Llamada recursiva con el acumulador actualizado.
+
+
+verificar_columna(IndiceColumna,PistasFilas,GrillaRes, 1) :-
+	nth0(IndiceColumna,PistasFilas,FiladePistas),
+	obtener_columna(GrillaRes,IndiceColumna,ColumnaDeGrilla),
+	verificar_pistas_en_lista(FiladePistas,ColumnaDeGrilla).
+
+verificar_columna(_,_,_,0).
+
+
+	%
+	% verificar_pistas_en_lista(+Pistas, +FiladeGrilla)
+	%
+/*
+CB: Si ya se descontaron las pistas dadas (hay 0 pistas) no tiene que haber #s en la lista
+CR: Si hay pistas y el primer elem de la lista es # entonces se verifica que cumpla si hay cierta cantidad de # consecutivos, si cumple luego se llama recursivamente 
+con las pistas restantes y la lista restante
+CR2: Si hay pistas y hay lista, y el 1er elem de la lista no es # entonces se llama recursivamente con las mismas pistas y la cola de la lista.
+*/
+
+verificar_pistas_en_lista([],ListaFila):-
+	not(member("#",ListaFila)).
+
+verificar_pistas_en_lista([X|PistasS], [Y|ListaFilaS]):-
+	Y == "#",
+	verificar_minimo_pconsecutivos(X, [Y|ListaFilaS], Restante),	%en caso de q se cumpla q haya p consecutivos retorna la lista restante
+	verificar_pistas_en_lista(PistasS, Restante).
+
+verificar_pistas_en_lista(Pistas, [Y|ListaFilaS]):- 
+	Y \== "#", 				   % Dada la lista de pistas, y el primer elemento de ListaFilaS (lista de fila)
+	verificar_pistas_en_lista(Pistas, ListaFilaS).
+
+
+%
+/*
+ verificar_minimo_pconsecutivos( +NumeroPista, +FilaARecorrer, -FilaRestante)
+ CB: si no hay mas pistas que verificar y no hay mas lista por recorrer ent las pistas se cumple
+ CB2: si no hay pista y si hay lista entonces si el primer elem de la lista no es # entonces cumple con que haya p #s consecutivos
+ CR: si hay pista entonces si el 1er elem de la lista es # entonces descontar pista y llamamos recursivamente con lista'.
+ lista' es lista sin su 1er elem 
+*/
+
+verificar_minimo_pconsecutivos(0,[],[]).	%													   
+
+verificar_minimo_pconsecutivos(0,[X|Filarestante],Filarestante):-
+	X \== "#".
+
+verificar_minimo_pconsecutivos(N,[X|Filarestante],Filarestante2):- 
+	X == "#", 
+	N > 0, 
+	Naux is N-1,   %
+	verificar_minimo_pconsecutivos(Naux,Filarestante,Filarestante2).
+
 
 /*
 
